a399a96be15462f11cfdd0dbdb2fe95f855f1610
tanhd <tanhd@rikkeisoft.com>
2018-04-17 10:07:02 +0700

    fix db and something

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,6 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->boolean('first_login')->default(false);
-            $table->json('category_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
