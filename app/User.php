d91a25be8f6f3380bcac2474ac5dbdc5c258498b
tanhd <tanhd@rikkeisoft.com>
2018-04-18 11:00:11 +0700

    create migration some talbe

--- app/User.php
+++ app/User.php
@@ -25,4 +25,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function roles()
+    {
+        return $this->belongsToMany('App\Role', 'users_roles');
+    }
 }
