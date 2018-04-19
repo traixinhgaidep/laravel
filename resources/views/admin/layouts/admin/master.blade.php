7301f75a5eeb1ac6becf5f14e3c54ef55365fca7
tanhd <tanhd@rikkeisoft.com>
2018-04-19 10:22:39 +0700

    adminlte redo

--- resources/views/admin/layouts/admin/master.blade.php
+++ resources/views/admin/layouts/admin/master.blade.php
@@ -5,10 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>SF-CMS</title>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <link rel="stylesheet" href="{{ asset('adminlte/bootstrap/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{ asset('adminlte/bower_components/font-awesome/css/font-awesome.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('adminlte/dist/css/AdminLTE.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('adminlte/dist/css/skins/_all-skins.min.css')}} ">
+    <link rel="stylesheet" href="{{ asset('css/adminlte/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ asset('css/adminlte/AdminLTE.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/adminlte/_all-skins.min.css')}} ">
     <link rel="stylesheet" href="{{ asset('https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.css') }}">
     @yield('css')
 </head>
@@ -100,7 +100,7 @@
 {{--var curRoute = '{{ $route }}';--}}
 </script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.1/jquery.min.js"></script>
-<script src="{{ asset('adminlte/dist/js/adminlte.min.js') }}"></script>
+<script src="{{ asset('js/adminlte/adminlte.min.js') }}"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.4.0/bootbox.min.js"></script>
 {{--<script src="{{ View::autoVersion('js/admin/main.js') }}"></script>--}}
