7f8f32c94cd9ff197176fb9ac3ad8e4350161939
dungnguyen96 <38413210+dungnguyen96@users.noreply.github.com>
2018-04-26 00:16:29 +0700

    add homepage2

--- resources/views/HomePage/detailpage.blade.php
+++ resources/views/HomePage/detailpage.blade.php
@@ -8,7 +8,7 @@
             <h3 ><b>{{$article->title}}<b></h3> 
                 <i>{{$article->created_at}}</i>
                 <img class="img-responsive"  src="{{asset($article->thumbnail) }}"/>
-                <p> <?= nl2br($article->content) ?> <p>
+                <!-- <p> <?= nl2br($article->content) ?> <p> -->
                 <p> <?=$article->content?><p>
                 </div>
             </div>
